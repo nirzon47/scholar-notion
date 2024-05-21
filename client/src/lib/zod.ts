dbedf153ee7f65527daeabf3827899a711800819
Nirzon Taru Karmakar <nirzonk47@gmail.com>
2024-05-21 22:36:01 +0530

    fix(client): zod schema error

--- client/src/lib/zod.ts
+++ client/src/lib/zod.ts
@@ -3,15 +3,15 @@ import { z } from 'zod'
 // Zod schemas
 export const LoginSchema = z.object({
 	email: z.string().email(),
-	password: z.string().min(6),
+	password: z.string().min(8),
 })
 
 export const SignupSchema = z
 	.object({
 		name: z.string(),
 		email: z.string().email(),
-		password: z.string().min(6),
-		confirmPassword: z.string().min(6),
+		password: z.string().min(8),
+		confirmPassword: z.string().min(8),
 		role: z.enum(['admin', 'teacher', 'student']),
 	})
 	.superRefine(({ confirmPassword, password }, ctx) => {
